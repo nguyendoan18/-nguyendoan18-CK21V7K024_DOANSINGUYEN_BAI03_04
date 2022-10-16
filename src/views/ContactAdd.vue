d8fa842115273cbea358e8ffeb095526dfaa5a82
DOAN SI NGUYEN <nguyendoan403@gmail.com>
2022-10-16 08:24:04 +0700

    Tao trang  them lienn  he

---
+++ src/views/ContactAdd.vue
@@ -0,0 +1,40 @@
+<template>
+  <div v-if="NguyenCK21V7K024" class="page">
+    <h4>Tạo mới Liên hệ</h4>
+    <ContactForm
+      :contact="contact"
+      @submit:contact="SubmitContact"
+    />
+    
+  </div>
+  <p>{{ message }}</p>
+</template>
+<script>
+import ContactForm from "@/components/ContactForm.vue";
+import ContactService from "@/services/contact.service";
+export default {
+  components: {
+    ContactForm,
+  },
+  data() {
+    return {
+      NguyenCK21V7K024: true,
+      contact: {},
+      message: ""
+    };
+  },
+  methods: {
+    
+    async SubmitContact(data) {
+      try {
+        await ContactService.create(data);
+        this.message = "Liên hệ được thêm thành công.";
+        this.NguyenCK21V7K024 = false;
+      } catch (error) {
+        console.log(error);
+      }
+    },
+   
+  },
+};
+</script>
